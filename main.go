4e3a9d7f6f6cb8d3b310f2a04047b75e7c7168bd
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-10-24 20:24:42 +0300

    Datatypes and reading input

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var conferenceName = "Go Conference"
 
 	const conferenceTickets = 50
@@ -11,9 +11,18 @@ func main(){
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 
-	fmt.Println("We have a total of",conferenceTickets,"and ",remainingTickets,"are still available")
+	fmt.Println("We have a total of", conferenceTickets, "and ", remainingTickets, "are still available")
 
 	fmt.Println("Get your tickets here to attend")
 
-	
+	var userName string
+	var userTickets int
+
+	// ask user for theri name
+
+	fmt.Scan(&userName)
+
+	userTickets = 20
+
+	fmt.Printf("User %v booked %v tickets. \m", userName, userTickets)
 }
