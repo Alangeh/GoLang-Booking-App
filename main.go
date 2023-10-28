f7f7284a2b4b326389a14b728312d2d00b654868
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-10-28 20:10:44 +0300

    infinite for loop

--- main.go
+++ main.go
@@ -19,28 +19,31 @@ func main() {
 	var booker []string     // slice
 	// booker := []string{} // alternative syntax for slice
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-
-	// ask user for theri name
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter your email")
-	fmt.Scan(&email)
-	fmt.Println("Enter Number of tickets")
-	fmt.Scan(&userTickets)
-
-	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
-	booker = append(booker, firstName+" "+lastName)
-
-	fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmtion email at %v\n", firstName, lastName, userTickets, email)
-
-	fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
-
-	fmt.Printf("These are all our boookings: %v\n", booker)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+	
+		// ask user for theri name
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your email")
+		fmt.Scan(&email)
+		fmt.Println("Enter Number of tickets")
+		fmt.Scan(&userTickets)
+	
+		remainingTickets = remainingTickets - userTickets
+		bookings[0] = firstName + " " + lastName
+		booker = append(booker, firstName+" "+lastName)
+	
+		fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmtion email at %v\n", firstName, lastName, userTickets, email)
+	
+		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+	
+		fmt.Printf("These are all our boookings: %v\n", booker)
+	}
+	
 }
