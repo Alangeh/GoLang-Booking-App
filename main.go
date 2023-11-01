b9fcd1c774c607a7f6bd8313a3591317c5481b3a
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-11-02 00:13:36 +0300

    using multiple files

--- main.go
+++ main.go
@@ -82,18 +82,6 @@ func getCity(cityCase int) string {
 	return city
 }
 
-func validateUserInput(
-	firstName string,
-	lastName string,
-	email string,
-	userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicket := userTickets > 0 && userTickets <= remainingTickets
-
-	return isValidName, isValidEmail, isValidTicket
-}
-
 func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
